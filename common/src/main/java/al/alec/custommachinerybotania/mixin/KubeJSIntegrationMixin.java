b7211cde42b4ab8572b0d0c0e41509d63dada596
alec016 <78179790+alec016@users.noreply.github.com>
2024-01-25 01:34:03 +0100

    added KubeJS Mixin integration

--- common/src/main/java/al/alec/custommachinerybotania/mixin/KubeJSIntegrationMixin.java
+++ common/src/main/java/al/alec/custommachinerybotania/mixin/KubeJSIntegrationMixin.java
@@ -1,3 +1,11 @@
 package al.alec.custommachinerybotania.mixin;
-public class KubeJSIntegrationMixin {
+
+import fr.frinn.custommachinery.common.integration.kubejs.CustomCraftRecipeJSBuilder;
+import fr.frinn.custommachinery.common.integration.kubejs.CustomMachineRecipeBuilderJS;
+import al.alec.custommachinerybotania.integration.kubejs.ManaRequirementJS;
+import al.alec.custommachinerybotania.integration.kubejs.ManaRequirementPerTickJS;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin({ CustomMachineRecipeBuilderJS.class, CustomCraftRecipeJSBuilder.class})
+public abstract class KubeJSIntegrationMixin implements ManaRequirementJS, ManaRequirementPerTickJS {
 }
